da3646038c4236ba3f34ec1b870b78eef18488db
Chen Su <420703660@qq.com>
2016-08-01 10:13:57 +0800

    solved it finally

---
+++ Basics/SPTTRN1.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+char matrix[100][100];
+
+bool isvaild(int x, int y, int size)
+{
+    int cnt = 0;
+    if(x+1 < size && matrix[x+1][y] == '*')
+        cnt++;
+    if(x-1 >= 0 && matrix[x-1][y] == '*')
+        cnt++;
+    if(y+1 < size && matrix[x][y+1] == '*')
+        cnt++;
+    if(y-1 >= 0 && matrix[x][y-1] == '*')
+        cnt++;
+    if(cnt > 1) return false;
+    return true;
+}
+
+int main()
+{
+    int linenum;
+    cin >> linenum;
+    for(int i = 0; i < linenum; i++)
+    {
+        int size;
+        cin >> size;
+        // initial
+        for(int i = 0; i < size; i++)
+            for(int j = 0; j < size; j++)
+                matrix[i][j] = '.';
+
+        int x = 0, y = 0, length = size;
+
+        if(length == 1)
+        {
+            cout  << '*' << endl;
+            return 0;
+        }
+        while(length > 0)
+         {
+            // top-left to top-right
+            for(int i = 0; i < length; i++)
+            {
+                if(isvaild(x, y+i, size))
+                    matrix[x][y+i] = '*';
+                else break;
+            }
+            // top-right to bottom-right
+            for(int i = 0; i < length; i++)
+            {
+                if(isvaild(x+i, y+length-1, size))
+                    matrix[x+i][y+length-1] = '*';
+                else break;
+            }
+            // bottom-right to bottom-left
+            for(int i = 0; i < length; i++)
+            {
+                if(isvaild(x+length-1, y+length-1-i, size))
+                    matrix[x+length-1][y+length-1-i] = '*';
+                else break;
+            }
+            // bottom-left to top-left
+            for(int i = 0; i < length; i++)
+            {
+                if(isvaild(x+length-1-i, y, size))
+                    matrix[x+length-1-i][y] = '*';
+                else break;
+            }
+            if(isvaild(x+2, y+1, size))
+                matrix[x+2][y+1] = '*';
+            else break;
+
+            x += 2;
+            y += 2;
+            length -= 4;
+        }
+
+        // output
+        for(int i = 0; i < size; i++)
+        {
+            for(int j = 0; j < size; j++)
+                cout << matrix[i][j];
+            cout << '\n';
+        }
+        cout << endl;
+    }
+    return 0;
+
+}
