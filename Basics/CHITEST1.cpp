1d8ab240a1cf52b5f7a986853fdfad031bb6f5c4
Chen Su <420703660@qq.com>
2016-08-02 15:30:26 +0800

    I don't say anything about it..

---
+++ Basics/CHITEST1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while(n--){
+        double a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
